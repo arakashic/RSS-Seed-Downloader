c37f507a5d2ba7740fce89a70af84590b66528e8
Flyxian <Flyxian@Asuka>
2011-09-15 00:52:05 -0600

    Load keyword list every time. i.e. live update.
    Change-Id: I25f4034dec797b38e03cdabf7b76e975431047f8
    Signed-off-by: Flyxian <Flyxian@Asuka>

--- src/main.py
+++ src/main.py
@@ -26,12 +26,13 @@ if __name__ == "__main__":
 #    print "main: Hello world."
     globals.write_log(0, "Starting...", "")
     globals.init_configs("test_config.yaml")
-    keyword_filter.init_keywords_list(globals.rss_config["keywords_list"])
+#    keyword_filter.init_keywords_list(globals.rss_config["keywords_list"])
     rss_parse.init()
     transmission_control.connect()
     
     globals.write_log(0, "Running...", "")
     while True:
+        keyword_filter.init_keywords_list(globals.rss_config["keywords_list"])
         ret = rss_parse.parse()
         if ret > 0:
             for seed in globals.seed_list:
